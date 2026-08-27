ef81de9c39f638477ba23164c5f1125733ee67e5
Alejimsan <chezmene@gmail.com>
2026-08-27 16:37:41 +0200

    Modify config.php

--- config.php
+++ config.php
@@ -1,9 +1,4 @@
 <?php
-// Configuración de la base de datos y AWS
-$db_user = "admin";
-$db_pass = "super_secret_password_123";
-
-// No borrar, necesario para el bucket S3
-$aws_access_key_id = "AKIAIOSFODNN7EXAMPLE";
-$aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+// Token falso para forzar detección de GitLeaks
+$github_token = "ghp_A1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8";
 ?>
