eb9073ec0584041d07828e895c81fe061f10bd82
Alejimsan <chezmene@gmail.com>
2026-08-27 16:31:37 +0200

    Create config.php for database and AWS settings
    Add database and AWS configuration settings.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Configuración de la base de datos y AWS
+$db_user = "admin";
+$db_pass = "super_secret_password_123";
+
+// No borrar, necesario para el bucket S3
+$aws_access_key_id = "AKIAIOSFODNN7EXAMPLE";
+$aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+?>
